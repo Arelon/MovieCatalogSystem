4777dff534376fad0c6206a24845e2d8be3fd776
Milan Aleksic <milanaleksic@gmail.com>
2012-03-17 09:39:39 +0100

    closes issue #73 (Update WinLauncher reference with new java-api)

--- src/net/milanaleksic/mcs/Startup.java
+++ src/net/milanaleksic/mcs/Startup.java
@@ -3,12 +3,12 @@ package net.milanaleksic.mcs;
 import net.milanaleksic.mcs.application.ApplicationManager;
 import net.milanaleksic.mcs.application.config.ProgramArgsService;
 import net.milanaleksic.mcs.infrastructure.util.VersionInformation;
+import net.milanaleksic.winlauncher.*;
 import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import java.io.*;
-import java.nio.channels.FileLock;
+import javax.swing.*;
 
 public class Startup {
 
@@ -16,20 +16,23 @@ public class Startup {
 
     public static void main(String[] args) {
         ProgramArgsService.setProgramArgs(args);
-        FileLock lock = null;
-        try {
-            lock = getSingletonApplicationFileLock();
-            if (log.isInfoEnabled())
-                log.info("Welcome to Movie Catalog System v" + VersionInformation.getVersion()+", booting application context..."); //NON-NLS
-            ApplicationContext applicationContext = bootSpringContext();
-            ApplicationManager applicationManager = ((ApplicationManager) applicationContext.getBean("applicationManager")); //NON-NLS
-            applicationManager.entryPoint();
-        } catch (Throwable t) {
-            log.error("Application error!", t); //NON-NLS
-        } finally {
-            if (lock != null)
-                closeSingletonApplicationLock(lock);
-        }
+
+        WinLauncherUtil.wrapSingletonApplicationLogic(
+                new ApplicationLogic(
+                        WinLauncherConfig.build().setExecutable("MCS.exe")
+                ) {
+                    public void run() {
+                        if (log.isInfoEnabled())
+                            log.info("Welcome to Movie Catalog System v" + VersionInformation.getVersion()+", booting application context..."); //NON-NLS
+                        ApplicationContext applicationContext = bootSpringContext();
+                        ApplicationManager applicationManager = ((ApplicationManager) applicationContext.getBean("applicationManager")); //NON-NLS
+                        applicationManager.entryPoint();
+                    }
+
+                    public void couldNotRun(Exception e) {
+                        JOptionPane.showMessageDialog(null, "Startup error - " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+                    }
+        });
     }
 
     private static ApplicationContext bootSpringContext() {
@@ -40,28 +43,4 @@ public class Startup {
         return context;
     }
 
-    private static FileLock getSingletonApplicationFileLock() {
-        final String lockFileName = ".launcher"; //NON-NLS
-        File locker = new File(lockFileName);
-        FileLock lock;
-        try {
-            if (!locker.exists())
-                throw new IllegalStateException("Nisam mogao da pristupim lock fajlu!");
-            lock = new RandomAccessFile(lockFileName, "rw").getChannel().tryLock(); //NON-NLS
-            if (lock == null) {
-                throw new IllegalStateException("Program je vec pokrenut, ne mozete pokrenuti novu instancu");
-            }
-        } catch (IOException e) {
-            throw new IllegalStateException("IO exception while trying to acquire lock");
-        }
-        return lock;
-    }
-
-    private static void closeSingletonApplicationLock(FileLock lock) {
-        try {
-            lock.channel().close();
-        } catch (IOException e) {
-            log.error(e);
-        }
-    }
 }
