ca4fb358a90f42108b74e0e62bfcad265b54ab6d
Milan Aleksic <milanaleksic@gmail.com>
2012-03-24 10:30:15 +0100

    closes #78 (Drop splash screen version writing)

--- src/net/milanaleksic/mcs/application/gui/helper/SplashScreenManager.java
+++ src/net/milanaleksic/mcs/application/gui/helper/SplashScreenManager.java
@@ -1,9 +1,8 @@
 package net.milanaleksic.mcs.application.gui.helper;
 
-import net.milanaleksic.mcs.infrastructure.config.UserConfiguration;
 import net.milanaleksic.mcs.infrastructure.LifecycleListener;
 import net.milanaleksic.mcs.infrastructure.config.ApplicationConfiguration;
-import net.milanaleksic.mcs.infrastructure.util.VersionInformation;
+import net.milanaleksic.mcs.infrastructure.config.UserConfiguration;
 
 import java.awt.*;
 
@@ -16,26 +15,13 @@ public class SplashScreenManager implements LifecycleListener {
 
     private SplashScreen splashScreen;
 
-    @Override public void applicationStarted(ApplicationConfiguration configuration, UserConfiguration userConfiguration) {
-        splashScreen = refreshSplashScreen();
+    @Override
+    public void applicationStarted(ApplicationConfiguration configuration, UserConfiguration userConfiguration) {
+        splashScreen = SplashScreen.getSplashScreen();
     }
 
-    @Override public void applicationShutdown(ApplicationConfiguration applicationConfiguration, UserConfiguration userConfiguration) {
-    }
-
-    private SplashScreen refreshSplashScreen() {
-        SplashScreen splashScreen = SplashScreen.getSplashScreen();
-        if (splashScreen != null) {
-            Graphics2D canvas = splashScreen.createGraphics();
-            canvas.setFont(new Font("Arial", Font.BOLD, 14));
-            canvas.setColor(Color.DARK_GRAY);
-            String text = "v" + VersionInformation.getVersion();
-            canvas.drawString(text,
-                    splashScreen.getBounds().width - canvas.getFontMetrics().stringWidth(text) - 10,
-                    splashScreen.getBounds().height - 10);
-            splashScreen.update();
-        }
-        return splashScreen;
+    @Override
+    public void applicationShutdown(ApplicationConfiguration applicationConfiguration, UserConfiguration userConfiguration) {
     }
 
     public void closeSplashScreen() {
