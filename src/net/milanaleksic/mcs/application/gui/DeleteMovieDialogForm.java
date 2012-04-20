ea10c9a6af0ba5edf15562cf0abd5f18c437270c
Milan Aleksic <milanaleksic@gmail.com>
2012-04-20 09:27:22 +0200

    reformatting code

--- src/net/milanaleksic/mcs/application/gui/DeleteMovieDialogForm.java
+++ src/net/milanaleksic/mcs/application/gui/DeleteMovieDialogForm.java
@@ -24,7 +24,8 @@ public class DeleteMovieDialogForm extends AbstractDialogForm {
 
     public static final String RESOURCE_ALERT_IMAGE = "/net/milanaleksic/mcs/application/res/alert.png"; //NON-NLS
 
-    @Inject private FilmRepository filmRepository;
+    @Inject
+    private FilmRepository filmRepository;
 
     private final static class AlertImagePainter implements PaintListener {
         public void paintControl(final PaintEvent e) {
@@ -41,91 +42,94 @@ public class DeleteMovieDialogForm extends AbstractDialogForm {
     public void open(Shell parent, Film film, Runnable callback) {
         this.film = Optional.of(film);
         super.open(parent, callback);
-	}
+    }
 
-	protected void reReadData() {
-		// preuzimanje podataka za film koji se azurira
+    protected void reReadData() {
+        // preuzimanje podataka za film koji se azurira
         if (film.isPresent()) {
             labFilmNaziv.setText(film.get().getNazivfilma() + "\n(" + film.get().getPrevodnazivafilma() + ")");
         }
-	}
-
-	@Override protected void onShellCreated() {
-		GridLayout gridLayout1 = new GridLayout();
-		gridLayout1.numColumns = 2;
-		shell.setText(bundle.getString("delete.deleteMovie"));
-		shell.setLayout(gridLayout1);
-		shell.setSize(new Point(431, 154));
-        	createCanvas();
-
-        	GridData gridData = new GridData();
-	        gridData.horizontalAlignment = org.eclipse.swt.layout.GridData.CENTER;
-	        gridData.grabExcessHorizontalSpace = true;
-	        Label labUpozorenje = new Label(shell, SWT.NONE);
-		labUpozorenje.setText(bundle.getString("delete.doYouReallyWishToDeleteMovie"));
-	        labUpozorenje.setLayoutData(gridData);
-	        Color color = new Color(Display.getCurrent(), 255, 0, 0);
-	        labUpozorenje.setForeground(color);
-	        color.dispose();
-	        labUpozorenje.setFont(new Font(Display.getDefault(), SWTUtil.getSystemFontData().getName(), 12, SWT.BOLD));
-
-	        GridData gridData2 = new GridData();
-	        gridData2.horizontalAlignment = org.eclipse.swt.layout.GridData.CENTER;
-	        gridData2.grabExcessVerticalSpace = true;
-	        labFilmNaziv = new Label(shell, SWT.WRAP | SWT.SHADOW_OUT | SWT.HORIZONTAL | SWT.CENTER);
-	        labFilmNaziv.setLayoutData(gridData2);
-		labFilmNaziv.setFont(new Font(Display.getDefault(), SWTUtil.getSystemFontData().getName(), 12, SWT.BOLD));
-		new Label(shell, SWT.NONE);
-		createComposite();
     }
 
-    @Override protected void onShellReady() {
-        reReadData();
-	}
+    @Override
+    protected void onShellCreated() {
+        GridLayout gridLayout1 = new GridLayout();
+        gridLayout1.numColumns = 2;
+        shell.setText(bundle.getString("delete.deleteMovie"));
+        shell.setLayout(gridLayout1);
+        shell.setSize(new Point(431, 154));
+        createCanvas();
 
-	private void createComposite() {
-		GridData gridData1 = new GridData();
-		gridData1.horizontalAlignment = org.eclipse.swt.layout.GridData.CENTER;
-		gridData1.grabExcessHorizontalSpace = true;
-		GridData gridData12 = new GridData();
-		gridData12.horizontalAlignment = GridData.END;
-		gridData12.grabExcessVerticalSpace = true;
-		GridLayout gridLayout = new GridLayout();
-		gridLayout.numColumns = 2;
-		gridLayout.horizontalSpacing = 20;
+        GridData gridData = new GridData();
+        gridData.horizontalAlignment = org.eclipse.swt.layout.GridData.CENTER;
+        gridData.grabExcessHorizontalSpace = true;
+        Label labUpozorenje = new Label(shell, SWT.NONE);
+        labUpozorenje.setText(bundle.getString("delete.doYouReallyWishToDeleteMovie"));
+        labUpozorenje.setLayoutData(gridData);
+        Color color = new Color(Display.getCurrent(), 255, 0, 0);
+        labUpozorenje.setForeground(color);
+        color.dispose();
+        labUpozorenje.setFont(new Font(Display.getDefault(), SWTUtil.getSystemFontData().getName(), 12, SWT.BOLD));
+
+        GridData gridData2 = new GridData();
+        gridData2.horizontalAlignment = org.eclipse.swt.layout.GridData.CENTER;
+        gridData2.grabExcessVerticalSpace = true;
+        labFilmNaziv = new Label(shell, SWT.WRAP | SWT.SHADOW_OUT | SWT.HORIZONTAL | SWT.CENTER);
+        labFilmNaziv.setLayoutData(gridData2);
+        labFilmNaziv.setFont(new Font(Display.getDefault(), SWTUtil.getSystemFontData().getName(), 12, SWT.BOLD));
+        new Label(shell, SWT.NONE);
+        createComposite();
+    }
+
+    @Override
+    protected void onShellReady() {
+        reReadData();
+    }
+
+    private void createComposite() {
+        GridData gridData1 = new GridData();
+        gridData1.horizontalAlignment = org.eclipse.swt.layout.GridData.CENTER;
+        gridData1.grabExcessHorizontalSpace = true;
+        GridData gridData12 = new GridData();
+        gridData12.horizontalAlignment = GridData.END;
+        gridData12.grabExcessVerticalSpace = true;
+        GridLayout gridLayout = new GridLayout();
+        gridLayout.numColumns = 2;
+        gridLayout.horizontalSpacing = 20;
         Composite composite = new Composite(shell, SWT.NONE);
-		composite.setLayout(gridLayout);
-		composite.setLayoutData(gridData1);
+        composite.setLayout(gridLayout);
+        composite.setLayoutData(gridData1);
         Button btnOk = new Button(composite, SWT.NONE);
-		btnOk.setText(bundle.getString("delete.confirmDeletion"));
+        btnOk.setText(bundle.getString("delete.confirmDeletion"));
         Button btnCancel = new Button(composite, SWT.NONE);
-		btnCancel.setText(bundle.getString("global.cancel"));
-		btnCancel.setLayoutData(gridData12);
-		btnOk.addSelectionListener(new HandledSelectionAdapter(shell, bundle) {
-            @Override public void handledSelected(SelectionEvent event) throws ApplicationException {
+        btnCancel.setText(bundle.getString("global.cancel"));
+        btnCancel.setLayoutData(gridData12);
+        btnOk.addSelectionListener(new HandledSelectionAdapter(shell, bundle) {
+            @Override
+            public void handledSelected(SelectionEvent event) throws ApplicationException {
                 filmRepository.deleteFilm(film.get());
                 runnerWhenClosingShouldRun = true;
                 shell.close();
             }
         });
-		btnCancel.addSelectionListener(new org.eclipse.swt.events.SelectionAdapter() {
+        btnCancel.addSelectionListener(new org.eclipse.swt.events.SelectionAdapter() {
             public void widgetSelected(org.eclipse.swt.events.SelectionEvent e) {
                 shell.close();
             }
         });
-	}
+    }
 
-	private void createCanvas() {
-		GridData gridData3 = new GridData();
-		gridData3.verticalSpan = 2;
-		gridData3.heightHint = 64;
-		gridData3.widthHint = 64;
+    private void createCanvas() {
+        GridData gridData3 = new GridData();
+        gridData3.verticalSpan = 2;
+        gridData3.heightHint = 64;
+        gridData3.widthHint = 64;
         Canvas canvas = new Canvas(shell, SWT.NONE);
-		canvas.setLayoutData(gridData3);
+        canvas.setLayoutData(gridData3);
 
-		Color bckg = canvas.getDisplay().getSystemColor(SWT.COLOR_WIDGET_BACKGROUND);
-		canvas.setBackground(bckg);
-		canvas.addPaintListener(new AlertImagePainter());
-	}
+        Color bckg = canvas.getDisplay().getSystemColor(SWT.COLOR_WIDGET_BACKGROUND);
+        canvas.setBackground(bckg);
+        canvas.addPaintListener(new AlertImagePainter());
+    }
 
 }
